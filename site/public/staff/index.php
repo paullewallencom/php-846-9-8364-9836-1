5b9b7379be0236a1abdcd29acba5a778d56e66ed
Paul Lewallen <paul.lewallen@gmail.com>
2019-06-24 16:08:53 -0700

    Update `index.php` to use `include` method.

--- site/public/staff/index.php
+++ site/public/staff/index.php
@@ -1,26 +1,7 @@
 <?php require_once('../../private/initialize.php'); ?>
 
-<!doctype html>
+<?php include('../../private/shared/staff_header.php'); ?>
 
-<html lang="en">
-<head>
-	<title>GBI</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" media="all" href="../stylesheets/staff.css" />
-</head>
-
-<body>
-	<header>
-		<h1>GBI Staff Area</h1>
-	<header>
-	<navigation>
-		<ul>
-			<li><a href="index.php">Menu</a></li>
-		</ul>
-	</navigation>
 	<div id="content"></div>
-	<footer>
-		&copy; <?php echo date('Y'); ?> Globe Bank
-	</footer>
-</body>
-</html>
+
+<?php include('../../private/shared/staff_footer.php'); ?>	
