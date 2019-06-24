4dabf082bf5f7d659186bc3db744a0c8735a2df2
Paul Lewallen <paul.lewallen@gmail.com>
2019-06-23 21:19:55 -0700

    Updated `staff`>`index.php`.

---
+++ site/public/staff/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+	<title>GBI</title>
+	<meta charset="utf-8">
+</head>
+
+<body>
+	<header>
+		<h1>GBI Staff Area</h1>
+	<header>
+	<navigation>
+		<ul>
+			<li><a href="index.php">Menu</a></li>
+		</ul>
+	</navigation>
+	<footer>
+		&copy; <?php echo date('Y'); ?> Globe Bank
+	</footer>
+</body>
+</html>
