e6ea34e9d72a2ee85746eb8084f41380cb848c4b
Paul Lewallen <paul.lewallen@gmail.com>
2019-06-30 10:43:44 -0700

    Update  to fix the  from before.

--- site/private/initialize.php
+++ site/private/initialize.php
@@ -8,6 +8,17 @@
   define("PUBLIC_PATH", PROJECT_PATH . '/public');
   define("SHARED_PATH", PRIVATE_PATH . '/shared');
 
+  // Assign the root URL to a PHP constant.
+  // * Do not need to include the domain.
+  // * Use same document root as webserver.
+  // * Can set a hardcoded value:
+  // define("WWW_ROOT", '/~YourUserName/globe_bank/public');
+  // define("WWW_ROOT", '');
+  // * Can dynamically find everything in URL up to "/public".
+  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
+  $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
+  define("WWW_ROOT", $doc_root);
+
   require_once('functions.php') 
 
 ?>
