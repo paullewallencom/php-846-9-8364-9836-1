142732f8ec10b0e660c807cf3d81206b75130ef1
Paul Lewallen <paul.lewallen@gmail.com>
2019-06-30 10:19:30 -0700

    Updated `staff_header.php` to echo the URL for `index.php`.

--- site/public/staff/index.php
+++ site/public/staff/index.php
@@ -3,6 +3,15 @@
 <?php  $page_title = 'Staff Menu'; ?>
 <?php include(SHARED_PATH . '/staff_header.php'); ?>
 
-	<div id="content"></div>
+	<div id="content">
+	  <div id="main-menu">
+	    <h2 style="color:black;">Main Menu</h2>
+	    <ul>
+	  	  <li>
+	  	    <a href="subjects/index.php">Subjects</a> 
+	  	  </li>
+	  	</ul>
+	  </div>
+	</div>
 
 <?php include(SHARED_PATH . '/staff_footer.php'); ?>	
