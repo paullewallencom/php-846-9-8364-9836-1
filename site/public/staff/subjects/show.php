1d9da23fa58aabda31152b21f9eaa1081e324c25
Paul Lewallen <paul.lewallen@gmail.com>
2019-06-30 14:09:20 -0700

    Updated 'View' link with deafult 'ID' for defensive programming.

--- site/public/staff/subjects/show.php
+++ site/public/staff/subjects/show.php
@@ -1,4 +1,18 @@
 <?php
-  $id = $_GET['id'];
+  // Generic Example: 
+  // $page = $_GET['page'];
+  // if(isset($_GET['page'])) {
+  //   $page = $_GET['page'];
+  // } else {
+  //   $page = '1';
+  // }
+  // $page = isset($_GET[;page']) ? $_GET['page'] : '1';
+  // $page = $_GET[;page'] ?? '1'; // PHP > 7.0
+  
+  // Implementation: PHP < 7.0
+  // $id = isset($_GET['id']) ? $_GET['id'] : '1';
+
+  $id = $_GET['id'] ?? '1'; // PHP > 7.0
+
   echo $id;
 ?>
