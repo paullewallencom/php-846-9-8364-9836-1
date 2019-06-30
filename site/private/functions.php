9f0f5c34f4a2636d323c3ccf744a7fef3c5e6a52
Paul Lewallen <paul.lewallen@gmail.com>
2019-06-30 15:27:09 -0700

    Updated 'show.php' to prevent injection into URL.

--- site/private/functions.php
+++ site/private/functions.php
@@ -8,4 +8,16 @@
   	return WWW_ROOT . $script_path;
   }
 
+  function u($string="") {
+  	return urlencode($string);
+  }
+
+  function raw_u($string="") {
+  	return rawurlencode($string);
+  }
+
+  function h($string="") {
+    return htmlspecialchars($string);
+  }
+
 ?>
--- site/public/staff/subjects/show.php
+++ site/public/staff/subjects/show.php
@@ -1,3 +1,5 @@
+<?php require_once('../../../private/initialize.php'); ?>
+
 <?php
   // Generic Example: 
   // $page = $_GET['page'];
@@ -12,7 +14,67 @@
   // Implementation: PHP < 7.0
   // $id = isset($_GET['id']) ? $_GET['id'] : '1';
 
+  // -----------
+  // | ! | %21 | 
+  // -----------
+  // | # | %23 | 
+  // -----------
+  // | $ | %24 | 
+  // -----------
+  // | % | %25 | 
+  // -----------
+  // | & | %26 | 
+  // -----------
+  // | ' | %27 | 
+  // -----------
+  // | ( | %28 | 
+  // -----------
+  // | ) | %29 | 
+  // -----------
+  // | * | %2A | 
+  // -----------
+  // | + | %2B | 
+  // -----------
+  // | , | %2C | 
+  // -----------
+  // | / | %2F | 
+  // -----------
+  // | : | %3A | 
+  // -----------
+  // | ; | %3B | 
+  // -----------
+  // | = | %3D | 
+  // -----------
+  // | ? | %3F | 
+  // -----------
+  // | @ | %40 | 
+  // -----------
+  // | [ | %5B | 
+  // -----------
+  // | ] | %5D | 
+  // -----------
+
+  // -------------
+  // | < | &lt;  | 
+  // -------------
+  // | > | &gt;  | 
+  // -------------
+  // | & | &amp; | 
+  // -------------
+  // | " | &quot | 
+  // -------------
+
+  // Injecting HTML tags into the URL.
+  // http://localhost/.../site/public/staff/subjects/show.php?id=1
+  // http://localhost/.../site/public/staff/subjects/show.php?id=<strong>1</strong>
+  // Injecting JavaScript into the URL.
+  // http://localhost/.../site/public/staff/subjects/show.php?id=<script>alert('Gotcha!');</script>
+
   $id = $_GET['id'] ?? '1'; // PHP > 7.0
 
-  echo $id;
+  echo h($id);
 ?>
+
+<a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br />
+<a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br />
+<a href="show.php?query=<?php echo u('!#*?'); ?>">Link</a><br />
